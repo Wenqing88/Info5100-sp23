82e587e98b1d844597e7ee1fae5d102d45ee9a8f
Wenqing88 <yang.wenqin@northeastern.edu>
2023-03-01 10:58:25 -0500

    [partial complete] `createPanel` and `User` class

---
+++ lab4/src/model/User.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+/**
+ *
+ * @author wenqing-mbp16
+ */
+public class User {
+    String name;
+    String employeeID;
+    int age;
+    String gender;
+    Date startDate;
+    String level;
+    long telephoneNumber;
+    String email;
+    String imagePath;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+   public Date getStartDate() {
+        return startDate;
+    }
+    
+    public void setStartDate(String myDate) {
+       
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date formattedDate = null;
+        try {
+            formattedDate = formatter.parse(myDate);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        this.startDate = formattedDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public long getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(long telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+    
+    
+    
+}
---
+++ lab4/src/model/UserDirectory.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author wenqing-mbp16
+ */
+import java.util.ArrayList;
+
+public class UserDirectory {
+    private ArrayList<User> allUsers;
+
+    public UserDirectory() {
+        this.allUsers = new ArrayList<>();
+    }
+
+    public ArrayList<User> getAllUsers() {
+        return allUsers;
+    }
+
+    public void setAllUsers(ArrayList<User> allUsers) {
+        this.allUsers = allUsers;
+    }
+    
+    public void addUser(User newUser) {
+        allUsers.add(newUser);
+    }
+    
+    public void removeUser(User user) {
+        allUsers.remove(user);
+    }
+}
+
+    
+    
+    
+    
+    
+    
+
